1447f8df79442307c6f9984470a157bcb6510577
alfacentaury <alfa.arnialfa@gmail.com>
2024-05-08 22:51:36 +0700

    Adjust appModule

--- app/src/main/java/com/centaury/simpletaskmanagement/di/AppModule.kt
+++ app/src/main/java/com/centaury/simpletaskmanagement/di/AppModule.kt
@@ -6,15 +6,21 @@ import com.centaury.simpletaskmanagement.data.db.NotesRepository
 import com.centaury.simpletaskmanagement.data.mapper.NotesResultMapper
 import com.centaury.simpletaskmanagement.di.module.DatabaseModule
 import com.centaury.simpletaskmanagement.domain.SimpleTaskRepository
+import com.centaury.simpletaskmanagement.ui.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule =
     module {
+        viewModel { MainViewModel(get()) }
+
         single { DatabaseModule().provideAppDatabase(get()) }
         single { DatabaseModule().provideMovieDao(get()) }
 
-        single<NotesEntityData> { NotesRepository(get()) }
-        single<SimpleTaskRepository> { SimpleTaskEntityRepository(get(), get()) }
+        factory<SimpleTaskRepository> { SimpleTaskEntityRepository(get(), get()) }
+        factory<NotesEntityData> { NotesRepository(get()) }
 
+        single { SimpleTaskEntityRepository(get(), get()) }
+        single { NotesRepository(get()) }
         single { NotesResultMapper() }
     }
